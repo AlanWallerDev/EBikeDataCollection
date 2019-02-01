29a5263e14107cb80b77c4b90afa8e10d308eb73
Alan Waller <zancar615@gmail.com>
2019-01-31 23:46:47 -0800

    created old session activity

---
+++ app/src/main/java/com/alan/waller/e_bikedatacollection/OldSessionActivity.java
@@ -0,0 +1,43 @@
+package com.alan.waller.e_bikedatacollection;
+
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
+
+public class OldSessionActivity extends AppCompatActivity {
+
+    private SessionViewModel sessionViewModel;
+
+    private SessionRecyclerViewAdapter sessionRecyclerViewAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.old_sessions_activity);
+
+        RecyclerView rv =(RecyclerView) findViewById(R.id.recyclerview);
+
+        sessionRecyclerViewAdapter = new SessionRecyclerViewAdapter(this);
+        rv.setAdapter(sessionRecyclerViewAdapter);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+
+        sessionViewModel = ViewModelProviders.of(this).get(SessionViewModel.class);
+
+        sessionViewModel.getAllSessions().observe(this, new Observer<List<Session>>() {
+            @Override
+            public void onChanged(@Nullable List<Session> sessions) {
+                sessionRecyclerViewAdapter.setSessions(sessions);
+            }
+        });
+
+    }
+
+}
